ec1ab726f899e35a956536605f73899c83cebcf4
8ohamed <114395272+8ohamed@users.noreply.github.com>
2022-10-13 15:41:18 +0200

    Update max.c

--- src/max.c
+++ src/max.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
 
-int max(int *numbers, int size) {
-
-  int max_value = 0;
-
-  for (int j = 0; j < size; j++) {
-    if (*numbers < *(numbers + j)) {
-      max_value = *(numbers + j);
-    }
-  }
-  return max_value;
+int max(int* numbers, int size)
+{
+//if statement to make sure that size is higher than 0, as stated in precondition//
+int x = numbers[0];
+assert(size > 0);
+//Counter to find the highest number in the array//
+for (int c=0; c < size; c++)
+{
+    if (numbers[c] > x)
+        x = numbers[c];
 }
-
+    return x;
+}
